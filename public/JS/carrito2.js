8a6c509909a5c7139b4967a77f83d846b2a6ffb9
in1dam-b <in1dam-b@elorrieta.int>
2021-05-26 17:42:01 +0200

    Control de stock part2

--- public/JS/carrito2.js
+++ public/JS/carrito2.js
@@ -1,75 +1,3 @@
-function AnadirCo(){
-	var unidades = parseInt(localStorage.getItem('osasunac'));
-	var Anadido = parseInt(localStorage.getItem('osasunaca')) + 1;
-
-
-	if(Anadido > unidades){
-		alert('No se puede añadir mas por que no hay stock disponible');
-	} else {
-		localStorage.setItem('osasunaca', Anadido);
-	}
-}
-
-function AnadirPo(){
-	var unidades = parseInt(localStorage.getItem('osasunap'));
-	var Anadido = parseInt(localStorage.getItem('osasunapa')) + 1;
-
-
-	if(Anadido > unidades){
-		alert('No se puede añadir mas por que no hay stock disponible');
-	} else {
-		localStorage.setItem('osasunapa', Anadido);
-	}
-}
-
-function AnadirCbo(){
-	var unidades = parseInt(localStorage.getItem('olivoc'));
-	var Anadido = parseInt(localStorage.getItem('olivoca')) + 1;
-
-
-	if(Anadido > unidades){
-		alert('No se puede añadir mas por que no hay stock disponible');
-	} else {
-		localStorage.setItem('olivoca', Anadido);
-	}
-}
-
-function AnadirPbo(){
-	var unidades = parseInt(localStorage.getItem('olivop'));
-	var Anadido = parseInt(localStorage.getItem('olivopa')) + 1;
-
-
-	if(Anadido > unidades){
-		alert('No se puede añadir mas por que no hay stock disponible');
-	} else {
-		localStorage.setItem('olivopa', Anadido);
-	}
-}
-
-function AnadirCt(){
-	var unidades = parseInt(localStorage.getItem('tipic'));
-	var Anadido = parseInt(localStorage.getItem('tipica')) + 1;
-
-
-	if(Anadido > unidades){
-		alert('No se puede añadir mas por que no hay stock disponible');
-	} else {
-		localStorage.setItem('tipica', Anadido);
-	}
-}
-
-function AnadirPt(){
-	var unidades = parseInt(localStorage.getItem('tipip'));
-	var Anadido = parseInt(localStorage.getItem('tipipa')) + 1;
-
-
-	if(Anadido > unidades){
-		alert('No se puede añadir mas por que no hay stock disponible');
-	} else {
-		localStorage.setItem('tipipa', Anadido);
-	}
-}
-
 function AnadirCs(){
 	var unidades = parseInt(localStorage.getItem('sidreriac'));
 	var Anadido = parseInt(localStorage.getItem('sidreriaca')) + 1;
@@ -141,3 +69,4 @@ function AnadirPb(){
 		localStorage.setItem('berriaca', Anadido);
 	}
 }
+
